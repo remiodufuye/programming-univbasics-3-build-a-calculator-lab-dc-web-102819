37b6b0f83af9c9ad9c58fd590bfdd7e06dfc499a
remi.odufuye <remi.odufuye@gmail.com>
2019-10-12 18:14:59 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -1,17 +1,25 @@
 def addition(num1, num2)
-
+ result = num1 + num2
+ puts result
+ result 
 end
 
 def subtraction(num1, num2)
-
+ result = num1 - num2
+ puts result
+ result 
 end
 
 def division(num1, num2)
-
+ result = num1 / num2
+ puts result
+ result
 end
 
 def multiplication(num1, num2)
-
+   result = num1 *  num2
+  puts result
+  result 
 end
 
 def modulo(num1, num2)
