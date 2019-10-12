c98ed574ebc95e8cee7ae3dcf8564213b64cd843
remi.odufuye <remi.odufuye@gmail.com>
2019-10-12 18:16:10 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -23,11 +23,13 @@ def multiplication(num1, num2)
 end
 
 def modulo(num1, num2)
-   result = num1 % num2
+  result = num1 % num2
   puts result
   result 
 end
 
 def square_root(num)
-
+  result = Math.sqrt(num)
+  puts result
+  result 
 end
